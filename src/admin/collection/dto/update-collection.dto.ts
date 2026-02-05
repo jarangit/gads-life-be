5578d80593fba7782697fc4a0b82749c04a8a189
jaran-ct <jaran.d@chareontut.co.th>
2026-02-05 14:43:57 +0700

    feat: add nameTh property with validation to UpdateCollectionDto

--- src/admin/collection/dto/update-collection.dto.ts
+++ src/admin/collection/dto/update-collection.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCollectionDto } from './create-collection.dto';
+import { IsString, MinLength } from 'class-validator';
 
-export class UpdateCollectionDto extends PartialType(CreateCollectionDto) {}
+export class UpdateCollectionDto extends PartialType(CreateCollectionDto) {
+  @IsString()
+  @MinLength(2)
+  nameTh: string;
+}
