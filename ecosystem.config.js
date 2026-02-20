03aa420231d01fd92f5c6fd5b6bc42c4c434846f
Jaran <Jaran.d@chareontut.co.th>
2026-02-20 14:32:24 +0700

    feat: add ecosystem configuration for application management

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'gads-life-be',
+      script: 'dist/main.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3001,
+      },
+      env_development: {
+        NODE_ENV: 'development',
+        PORT: 3001,
+      },
+    },
+  ],
+};
